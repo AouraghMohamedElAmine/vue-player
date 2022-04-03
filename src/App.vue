1100df3b7c3df3ec6e1270c4eaec4acd5fa4f05b
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2022-04-03 10:58:15 -0300

    Refactor project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-row justify="center">
-        <v-col md="6">
-          <video-player />
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+  <v-container>
+    <v-row justify="center">
+      <v-col md="6">
+        <video-player :videoURL="videoURL" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import VideoPlayer from './components/VideoPlayer'
+import VideoPlayer from "./components/VideoPlayer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    VideoPlayer
-  }
-}
-
+    VideoPlayer,
+  },
+  data: () => ({
+    videoURL:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+  }),
+  methods: {
+    changeVideoPlaying(url) {
+      this.videoURL = url;
+    },
+  },
+};
 </script>
+
+<style>
+body {
+  font-family: Roboto;
+}
+</style>
